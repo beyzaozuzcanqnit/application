8ff1ce318100c59d363ae132c56a07ed22f84ba0
saikrishna <saikrishna.4369@gmail.com>
2023-03-02 15:39:37 +0100

    userlogin page

---
+++ cypress/support/pageObjects/UserLoginPage.js
@@ -0,0 +1,25 @@
+class UserLoginPage {
+    login(userName, userLoginPassword) {
+      this.userName = typeof userName !== 'undefined' ? userName : '';
+      this.loginPassowrd = typeof userLoginPassword !== 'undefined' ? userLoginPassword : '';
+  
+      cy.visit(Cypress.env('loginPage_url'));
+  
+      // Typing in login userName
+      if (this.userName.length === 0) {
+        cy.get('#username').invoke('val', '');
+      } else if (this.userName.length > 0) {
+        cy.get('#username').type(this.userName);
+      }
+  
+      // Typing in login password
+      if (this.loginPassowrd.length === 0) {
+        cy.get('#password').invoke('val', '');
+      } else if (this.loginPassowrd.length > 0) {
+        cy.get('#password').type(this.loginPassowrd);
+      }
+    }
+  }
+  
+  export default UserLoginPage;
+  
