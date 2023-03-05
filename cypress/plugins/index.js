48539b00895edee3438948191471fc53b4d0017e
saikrishna <saikrishna.4369@gmail.com>
2023-03-05 17:33:22 +0100

    update

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -19,13 +19,4 @@
 const cucumber = require("cypress-cucumber-preprocessor").default;
 module.exports = (on, config) => {
   on("file:preprocessor", cucumber());
-  on("after:spec", (spec, results) => {
-    if (results.stats.failures === 0 && results.video) {
-      // `del()` returns a promise, so it's important to return it to ensure
-      // deleting the video is finished before moving on
-      return del(results.video);
-    }
-
-    return Promise.resolve(false);
-  });
 };
